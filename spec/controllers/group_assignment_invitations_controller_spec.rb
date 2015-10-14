578b4a8f9a4a1455da397e6b708d65af79a12322
Mark Tareshawty <mark@marktareshawty.com>
2015-10-14 13:30:08 -0400

    group assignment invitations

--- spec/controllers/group_assignment_invitations_controller_spec.rb
+++ spec/controllers/group_assignment_invitations_controller_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe GroupAssignmentInvitationsController, type: :controller do
       it 'redeems the users invitation' do
         patch :accept_invitation, id: invitation.key, group: { title: 'Code Squad' }
 
-        assert_requested :post, github_url("/organizations/#{organization.github_id}/teams"), times: 2
+        assert_requested :post, github_url("/organizations/#{organization.github_id}/teams")
         assert_requested :post, github_url("/organizations/#{organization.github_id}/repos")
 
         expect(group_assignment.group_assignment_repos.count).to eql(1)
